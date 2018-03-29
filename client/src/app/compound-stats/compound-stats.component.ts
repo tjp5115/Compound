d5e6358cd2050f63e956510bff2e245c8cefc09e
Tyler Paulsen <tpaulsen88@gmail.com>
2018-03-29 10:58:00 -0400

    Stat UI update to search Stock and what stat.

--- client/src/app/compound-stats/compound-stats.component.ts
+++ client/src/app/compound-stats/compound-stats.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compound-stats.component.css']
 })
 export class CompoundStatsComponent implements OnInit {
-
+  public statOptions = [ 'yearOverYear' ];
   constructor() { }
 
   ngOnInit() {
