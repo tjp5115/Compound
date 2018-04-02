c0cef4ee3220d051392d53a1a1f0de9e3fc1a1cd
Tyler Paulsen <tpaulsen88@gmail.com>
2018-04-01 22:44:32 -0400

    Consume Anova service.

--- client/src/app/compound-stats/compound-stats.component.ts
+++ client/src/app/compound-stats/compound-stats.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CompoundStatsService } from '../shared/stock/compound-stats.service';
 @Component({
   selector: 'app-compound-stats',
   templateUrl: './compound-stats.component.html',
-  styleUrls: ['./compound-stats.component.css']
+  styleUrls: ['./compound-stats.component.css'],
+  providers: [CompoundStatsService]
 })
 export class CompoundStatsComponent implements OnInit {
   public statOptions = [ 'yearOverYear' ];
-  constructor() { }
+  public symbol : string;
+  public anova : Number
+
+  constructor(private statService: CompoundStatsService) { }
 
   ngOnInit() {
+  }
+
+  searchSymbol(){
+
+    this.statService.getSymbol(this.symbol).subscribe(
+      data => {
+      	this.anova = data.oneWayAnova;
+	   },
+      error => console.log(error)
+    )
   }
 
 }
